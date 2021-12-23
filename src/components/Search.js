dc2d5a2befb80f1c0a594d22b03b651154bb9232
Tuba Özyurt <tuba.ozyurt93@gmail.com>
2021-12-23 23:35:29 +0300

    .

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
-/eslint array-callback-return: "error"/
+/*eslint array-callback-return: "error"*/
+
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios';
